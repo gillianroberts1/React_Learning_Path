de36b61941dc4e49b8406b1e0ddfb8e8ccf4fa96
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2024-09-24 14:57:37 +0100

    further if else functions

--- Javascript_Basics/1-Control_Flow/task.js
+++ Javascript_Basics/1-Control_Flow/task.js
@@ -17,8 +17,6 @@ function checkAge(age) {
 console.log(checkAge(16)); // Output: "You are a minor."
 console.log(checkAge(20)); // Output: "You are an adult."
 
-
-
 // Check to see if the array contains weekdays
 // "This is a weekday" if the day is during the week
 // "This is a weekend" if the day is not included in the array of weekdays
@@ -33,8 +31,54 @@ function weekday(day) {
   }
 }
 
-console.log(weekday('saturday'));  // output - "This is a weekend"
+console.log(weekday('saturday')); // output - "This is a weekend"
 console.log(weekday('tuesday')); // Output "This is a weekday"
+
+// check to see if your tel no valid by being 11 digits long
+// "Your tel no is not valid" if it is more or less than 11 digits
+// "Your tel no is valid" if it is exactly 11 digits long
+
+function checkValidTelNo(telno) {
+  if (telno.length == 11) {
+    console.log('Your tel no is Valid');
+  } else {
+    console.log('Your tel no is not valid');
+  }
+}
+
+console.log(checkValidTelNo('07825959795')); // Output "Your tel no is valid"
+console.log(checkValidTelNo('0876')); // Output "Your tel no is not valid"
+console.log(checkValidTelNo('08763355442222')); // Output "Your tel no is not valid"
+
+// check to see if a name contains the letter C
+// "This name does not contain a C" if there is no C in the name
+// "This name does contain a C" if there is a C in the name
+
+function checkForC(name) {
+  if (name.includes('c')) {
+    console.log('This name does include a c');
+  } else {
+    console.log('This name does not include a c');
+  }
+}
+
+console.log(checkForC('carmen')); // Output "This name does include a c"
+console.log(checkForC('gillian')); // Output "This name does not include a c"
+
+// check to see if a country starts with the letter S
+// "this country starts with a S"
+// "this country does not start with a letter S"
+
+function startsWithS(country) {
+  if (country.startsWith("S")) {
+    console.log('This country starts with S');
+  } else {
+    console.log('This country does not start with S');
+  }
+}
+
+console.log(startsWithS("Scotland"));// Output "This country does start with S"
+console.log(startsWithS("England"));// Output "This country does not start with S"
 
 
 
